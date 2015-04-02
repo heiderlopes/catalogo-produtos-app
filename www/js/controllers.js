0c7e72ed714f51e60deb7257cae1b79c4c317695
Heider Lopes <heiderlopes@MacBook-Pro-de-Heider.local>
2015-04-02 02:43:45 -0300

    Commit inicial

---
+++ www/js/controllers.js
@@ -0,0 +1,20 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  
+})
+
+.controller('servicoCtrl', function($scope, $http) {
+
+  $scope.doRefresh = function () {
+    $http.get('https://apricot-surprise-2279.herokuapp.com/api/products',
+      {cache: true})
+    .then(function(response) {
+      $scope.servicos = {};
+      $scope.servicos = response.data;
+      console.log(response.data);
+      $scope.$broadcast('scroll.refreshComplete');
+    })
+  };
+
+});
